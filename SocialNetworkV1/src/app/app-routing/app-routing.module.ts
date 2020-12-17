cae7ed189a66d05634e9da6dfd96440d0c7072ec
SieraSD <sierastrau@gmail.com>
2020-12-16 21:14:25 -0500

    routing works

--- SocialNetworkV1/src/app/app-routing/app-routing.module.ts
+++ SocialNetworkV1/src/app/app-routing/app-routing.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { RegisterComponent } from '../register/register.component';
+import { LandingPageComponent } from '../landing-page/landing-page.component';
 
-const routes: Routes = [{path: 'login',component: LoginComponent}, 
-                        {path: 'register', component: RegisterComponent}];
+const routes: Routes = [{path: '',pathMatch: 'full', redirectTo: 'landing-page'}, 
+                        {path: 'landing-page', component: LandingPageComponent},
+                        {path: 'register', component: RegisterComponent},
+                      {path: 'login', component: LoginComponent}];
 
 @NgModule({
 
